91d13d423af1b309e9547b4523394fea40718a8d
Ekansh Khandekar <ekansh.khandekar.work@gmail.com>
2025-11-12 19:58:43 +0530

    input array and display the array

--- Basics.java
+++ Basics.java
@@ -1,38 +1,22 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Basics {
-   public int[] plusOne(int[] digits) {
-    int n = digits.length;
-    for(int i = n-1; i >= 0; i--) {
-        if(digits[i] < 9) {
-            digits[i]++;
-            return digits;
+    public void displayArray() {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+
+        for(int i = 0; i<size; i++) {
+            nums[i] = sc.nextInt();    
         }
-
-        // digits[i] = 0;
-    }
-
-    int[] result = new int[n+1];
-    result[0] = 1;
-
-
-    return result;
+        for(int num: nums) {
+            System.out.println(num);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        int[] nums = {1,2,3,4};
-
-
         Basics obj = new Basics();
-        // List<Integer> li = obj.spiralOrder(nums1);
-        int[] k = obj.plusOne(nums);
-        System.out.println("result :" + Arrays.toString(k));
+        obj.displayArray();
     }
     
 }
