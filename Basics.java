aa4e8c20d6342ba5041e97bbf3ced551170d8339
Ekansh Khandekar <ekansh.khandekar.work@gmail.com>
2025-11-13 18:37:02 +0530

    Added comments

--- Basics.java
+++ Basics.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class Basics {
     public void displayArray() {
         Scanner sc = new Scanner(System.in);
+        //Input size of array
         int size = sc.nextInt();
         int[] array = new int[size];
-
+        
+        //Input array elements
         for(int i = 0; i<size; i++) {
             array[i] = sc.nextInt();    
         }
+
+        //Display array elements
         for(int num: array) {
             System.out.println(num);
         }
     }
 
     public static void main(String[] args) {
-        Basics obj = new Basics();
-        obj.displayArray();
+        Basics basics = new Basics();
+        basics.displayArray();
     }
     
 }
